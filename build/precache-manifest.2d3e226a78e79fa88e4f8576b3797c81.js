a248a3fa4c70c4a8116ce7dd77dbee1d301947f2
Sara <saracelalfonso@gmail.com>
2021-03-15 20:28:18 +0100

    add build

---
+++ build/precache-manifest.2d3e226a78e79fa88e4f8576b3797c81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb151e9191234bf1a9043e722bc66abf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3bfbfa5549f166afb76b",
+    "url": "/static/css/main.011a45a9.chunk.css"
+  },
+  {
+    "revision": "646d68bd1c93815a5795",
+    "url": "/static/js/2.3d232d3c.chunk.js"
+  },
+  {
+    "revision": "9a2cf355ca348d84eedb0129142dc576",
+    "url": "/static/js/2.3d232d3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bfbfa5549f166afb76b",
+    "url": "/static/js/main.eef3f29a.chunk.js"
+  },
+  {
+    "revision": "7eb8d6388ecf90523c80",
+    "url": "/static/js/runtime-main.7dc31f0f.js"
+  }
+]);
